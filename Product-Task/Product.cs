09446b9ca662438748785edd4f3ae24aabd4a6de
unknown <takacstamas@terem307pc7.athenepraxis.local>
2017-09-11 18:22:39 +0200

    Second Commit, Main Window + Buttons

---
+++ Product-Task/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Task
+{
+    public class Product
+    {
+        string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+              //  OnPropertyChange();
+            }
+        }
+        int _price;
+        public int Price
+        {
+            get { return _price; }
+            set
+            {
+                _price = value;
+                //OnPropertyChange();
+            }
+        }
+
+        int _quantity;
+        public int Quantity
+        {
+            get { return _quantity; }
+            set
+            {
+                _price = value;
+                //OnPropertyChange();
+            }
+        }
+    }
+}
